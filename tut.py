a87405048cb0f4fe1e1b32658fb37e1afaee9397
Ondrej Certik <ondrej@certik.cz>
2009-08-17 18:03:58 -0600

    factoring added

--- tut.py
+++ tut.py
@@ -1,3 +1,5 @@
+# INTEGRATION
+
 from sympy import *
 var("x y z")
 e = Integral(sin(x), (x, 0, pi))
@@ -6,3 +8,22 @@ f = lambdify(e.args[1][0][0], e.args[0])
 quad(f, e.args[1][0][1][0], e.args[1][0][1][1])
 
 # see the bug, show how to fix it.
+
+# FACTORING
+
+In [1]: e = (x+y+z)**2
+
+In [2]: e
+Out[2]: 
+           2
+(x + y + z) 
+
+In [3]: e.expand()
+Out[3]: 
+                         2    2    2
+2⋅x⋅y + 2⋅x⋅z + 2⋅y⋅z + x  + y  + z 
+
+In [4]: factor(e.expand())
+Out[4]: 
+           2
+(x + y + z) 
