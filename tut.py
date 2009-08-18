957fa895fcb94dc35f17ff9ef7918cdb91b1fb43
Ondrej Certik <ondrej@certik.cz>
2009-08-18 10:12:36 -0700

    more stuff added

--- tut.py
+++ tut.py
@@ -1,5 +1,5 @@
 from sympy import *
-var("x y z")
+var("x y z n")
 
 # FACTORING
 
@@ -90,3 +90,96 @@ M = Matrix(4, 4, lambda i,j: i*j+3)
 M.eigenvals()
 M = Matrix(4, 4, lambda i,j: i*j+x)
 M.eigenvals()
+
+# General relativity example
+
+# $ examples/advanced/relativity.py
+
+
+# Integrals table
+
+from sympy import *
+
+def derivative_table(functions, x):
+    for f in functions:
+        s = printing.latex(Eq(Derivative(f, x), diff(f, x)))
+        print ":<math>" + s[1:-1] + "</math>", "\n"
+
+def integral_table(functions, x):
+    for f in functions:
+        s = printing.latex(Eq(Integral(f, x), integrate(f, x)))
+        print ":<math>" + s[1:-1] + "</math>", "\n"
+
+var('x')
+
+print "===Derivatives==="
+derivative_table([cos(x)/(1 + sin(x)**i) for i in range(1, 5)], x)
+
+print "===Integrals==="
+integral_table([x**i * exp(i*x) for i in range(1, 5)], x)
+
+
+# Million digits of pi
+
+a = pi.evalf(10**6)
+len(str(a))
+str(a)[:50]
+
+
+# Numerical Integration
+
+# One-liner:
+
+Integral(sin(1/x), (x, 0, 1)).transform(x, 1/x).evalf(quad="osc")
+# 0.504067061906928
+
+# Detailed steps:
+
+e = Integral(sin(1/x), (x, 0, 1))
+e
+e.transform(x, 1/x)
+e.transform(x, 1/x).evalf(quad="osc")
+e.transform(x, 1/x).evalf(40, quad="osc")
+
+# Numerical Summation
+
+# quickly convergent series:
+
+Sum((2*n**3+1)/factorial(2*n+1), (n, 0, oo)).evalf(1000)
+
+# slowly convergent (polynomial rate) series:
+
+Sum(n/(n**3+9), (n, 1, oo)).evalf(1000)
+
+# Numerical Simplification
+
+a = float(1/7)
+nsimplify(a)
+a = float(1/81)
+nsimplify(a)
+nsimplify(pi, tolerance=0.01)
+nsimplify(pi, tolerance=0.001)
+nsimplify(0.33333, tolerance=1e-4)
+nsimplify(4.71, [pi], tolerance=0.01)
+nsimplify(2.0**(1/3.), tolerance=0.001)
+nsimplify(2.0**(1/3.), tolerance=0.001, full=True)
+nsimplify(4/(1+sqrt(5)), [GoldenRatio])
+nsimplify(2 + exp(2*atan('1/4')*I))
+nsimplify((1/(exp(3*pi*I/5)+1)))
+nsimplify(I**I, [pi])
+nsimplify(Sum(1/n**2, (n, 1, oo)), [pi])
+nsimplify(gamma('1/4')*gamma('3/4'), [pi])
+
+# Ordinary differential equations
+
+f = Function("f")
+e = Eq(f(x).diff(x, x) + 9*f(x) + 1, 1)
+dsolve(e, f(x))
+
+e = Eq(f(x).diff(x, x) + 8*f(x) + 1, 1)
+dsolve(e, f(x))
+
+
+# Limits
+
+# $ vim sympy/series/tests/test_demidovich.py
